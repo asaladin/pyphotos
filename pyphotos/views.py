27804e4f8faab279300409566829a3b47fa5baa4
Adrien Saladin <adrien.saladin@gmail.com>
2012-01-15 15:03:04 +0100

    now works with PIL and BytesIO

--- pyphotos/views.py
+++ pyphotos/views.py
@@ -3,7 +3,7 @@ from pyramid.httpexceptions import HTTPFound
 import boto
 import time
 import Image
-
+from io import BytesIO
 
 def my_view(request):
     albums = request.db.albums.find({'visible': True})
@@ -15,7 +15,9 @@ def listalbum(request):
     photos = request.db.photos.find({'album': albumname})
     
     return {'albumname': albumname, 'photos': photos}
-    
+
+
+
     
 def addphotoform(request):
     
@@ -33,9 +35,20 @@ def addphotoform(request):
         key.set_contents_from_file(inputfile)
         
         inputfile.seek(0)
+        size = 128, 128
+        im = Image.open(inputfile)
+        im.thumbnail(size, Image.ANTIALIAS)
         
+        imagefile = BytesIO()
+        def fileno():
+            raise AttributeError
+        imagefile.fileno = fileno #hack to make PIL and BytesIO work together...
+                
+        im.save(imagefile, 'JPEG')
         
-        request.db.photos.insert({'album': albumname, 'filename': filename})
+        imagefile.seek(0)
+        file_id = request.fs.put(imagefile, filename=filename)
+        request.db.photos.insert({'album': albumname, 'filename': filename, 'thumbnailid': file_id})
         
                 
         return HTTPFound("/album/%s/addphoto"%albumname)
