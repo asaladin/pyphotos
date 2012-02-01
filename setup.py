bd6a1f45ba4bf7eb2e9bcbfae46ed4496e722a0a
Adrien Saladin <adrien.saladin@gmail.com>
2012-02-01 18:01:31 +0100

    added dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-requires = ['pyramid', 'pyramid_debugtoolbar', 'boto', 'pyramid_beaker']
+requires = ['pyramid', 'pyramid_debugtoolbar', 'boto', 'pyramid_beaker', 'velruse', 'pymongo', 'PIL']
 
 setup(name='pyphotos',
       version='0.0',
