29cfd377d4397736adca3c4e901a51951a129d09
Adrien Saladin <adrien.saladin@gmail.com>
2012-02-04 15:01:27 +0100

    allow album creation for authenticated users only

--- pyphotos/resources.py
+++ pyphotos/resources.py
@@ -1,8 +1,11 @@
-from pyramid.security import Everyone
+from pyramid.security import Everyone, Authenticated
 from pyramid.security import Allow, Deny
 
 class Root(object):
     def __init__(self, request):
         self.request = request
-        self.__acl__ = [(Allow, 'toto', 'view'),]
+        self.__acl__ = [
+                         (Allow, Authenticated, 'create'),
+                         (Allow, 'toto', 'view'),
+                       ]
     
