b6512298b4c96a055041366ec6118f83dd50dec9
Adrien Saladin <adrien.saladin@gmail.com>
2013-08-27 15:42:10 +0200

    code cleaning

--- pyphotos/__init__.py
+++ pyphotos/__init__.py
@@ -58,28 +58,17 @@ def main(global_config, **settings):
     
     config.add_subscriber(add_store, NewRequest)
     config.add_subscriber(check_for_new_user, NewRequest)
-    
-    #add root account if none present:
-    #if db.users.find({'admin':True}).count() == 0:
-    #    pwd = hashlib.sha1('%s'%random.randint(1,1e99)).hexdigest()
-    #    
-    #    db.users.insert({'login':'root', 'pwd':pwd, 'admin':True })
-    #    print 'created root account with password ', pwd
-    
+
     
     config.add_route("index", "/")
     config.add_route("listalbum", "/album/{albumname}/list", factory="pyphotos.resources.AlbumFactory")
     config.add_route("addphotoform", "/album/{albumname}/addphoto", factory="pyphotos.resources.AlbumFactory")
     config.add_route("view_thumbnail", "/thumbnail/{albumname}/{filename}", factory="pyphotos.resources.AlbumFactory")
-    #config.add_route("login", "/login")
-    #config.add_route("browserid_login", "/login/browserid")
-    #config.add_route("logout", "/logout")
     config.add_route("newalbum", "/newalbum")
     config.add_route("createticket", "/createticket/{albumname}", factory="pyphotos.resources.AlbumFactory")
     config.add_route("allowview", "/allow/{credential}")
     config.add_route('myalbums', '/myalbums')
     config.add_route('fullsize', '/fullsize/{albumname}/{filename}', factory="pyphotos.resources.AlbumFactory")
-    #config.add_route('new_user', "/newuser")
     config.add_route('import_s3', '/import/s3')
     config.add_route('generate_thumbnail', '/thumbnail/generate/{albumname}/{filename}')
     config.add_route('admin', '/admin')
@@ -88,23 +77,25 @@ def main(global_config, **settings):
     
     config.add_forbidden_view(forbidden_view)
     
-
+    #add a 'user' attribute accessed by 'request.user' for each view and template:
     config.add_request_method('pyphotos.lib.get_user', name='user', property=True, reify=True)
     
     config.scan()
-    
+
+    #add a local user:
+    admin_email = config.registry.settings['admin_email']
+    nuser = DBSession.query(User).filter(User.email==admin_email).count()
+    if nuser==0:
+        user = User()
+        user.email = admin_email
+        user.username = config.registry.settings['admin_name']
+        DBSession.add(user)
+        transaction.commit()
+        log.debug("added admin user")
+
       
     if config.registry.settings['pyphotos_debug_mode']:
-        #add a local user:
-        nuser = DBSession.query(User).filter(User.email=="root@localhost").count()
-        if nuser==0:
-            user = User()
-            user.email = "root@localhost"
-            user.username = "root"
-            DBSession.add(user)
-            transaction.commit()
-  
- 
+        #add debug views:
         from .views import debug_login
         config.add_route('debug_login', '/login/debug')
         config.add_view(debug_login, route_name='debug_login')
